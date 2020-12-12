4a92410e7487854cade34dc76caf44c691f60ee4
Jemand Andere <jemandandere@gmail.com>
2020-12-12 22:25:44 +0300

    Добавили проверку тестов (assert)

--- src/test/java/com/example/Tester.java
+++ src/test/java/com/example/Tester.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import model.Vec2Int;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Tester {
@@ -36,26 +37,22 @@ public class Tester {
         Rect d = new Rect(new Vec2Int(3, 2), 2);
         Rect e = new Rect(new Vec2Int(9, 9), 1);
         Rect f = new Rect(new Vec2Int(4, 9), 1);
-        System.out.println(incec(a, b));
-        System.out.println(incec(a, c));
-        System.out.println(incec(a, d));
-        System.out.println(incec(b, c));
-        System.out.println(incec(b, d));
-        System.out.println(incec(c, d));
-        System.out.println(incec(c, e));
-        System.out.println(incec(c, f));
-        System.out.println(incec(e, f));
+        Assertions.assertEquals(true, incec(a, b));
+        Assertions.assertEquals(false, incec(a, c));
+        Assertions.assertEquals(true, incec(a, d));
+        Assertions.assertEquals(false, incec(b, c));
+        Assertions.assertEquals(false, incec(b, d));
+        Assertions.assertEquals(false, incec(c, d));
+        Assertions.assertEquals(false, incec(c, e));
+        Assertions.assertEquals(false, incec(c, f));
+        Assertions.assertEquals(false, incec(e, f));
     }
 
     static boolean incec(Rect a, Rect b) {
-        //System.out.println(a);
-        //System.out.println(b);
         Integer maxLeft = Math.max(a.getPos().getX(), b.getPos().getX());
         Integer minRight = Math.min(a.getPos().getX() + a.getSize(), b.getPos().getX() + b.getSize());
-        //System.out.println(maxLeft + " - " + minRight + " = " + (minRight - maxLeft));
         Integer maxBottom = Math.max(a.getPos().getY(), b.getPos().getY());
         Integer minTop = Math.min(a.getPos().getY() + a.getSize(), b.getPos().getY() + b.getSize());
-        //System.out.println(maxBottom + " - " + minTop + " = " + (minTop - maxBottom));
         return (Math.max(a.getPos().getX(), b.getPos().getX()) - Math.min(a.getPos().getX() + a.getSize(), b.getPos().getX() + b.getSize()) < 0) &&
                 (Math.max(a.getPos().getY(), b.getPos().getY()) - Math.min(a.getPos().getY() + a.getSize(), b.getPos().getY() + b.getSize()) < 0);
     }
